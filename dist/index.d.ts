091e17a73a30c7f34f0e2e84f425e445f63a6558
Adam Bibby <adam_bibby@hotmail.com>
2020-09-07 09:08:43 -0400

    remove dist folders

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { CliConfigOptions, Configuration, Entry, EntryFunc } from 'webpack';
-export interface Options {
-    entry: string | string[] | Entry | EntryFunc;
-    html: string;
-    moduleBase: string;
-    dist: string;
-    publicPath?: string;
-}
-export declare function build(options: Options): (env: string | Record<string, boolean | number | string>, argv: CliConfigOptions) => Configuration | Promise<Configuration>;
